eac7a4778d933a9ce6eb6a49c6431c02836c01c2
Masaba Gerald <masabagerald@gmail.com>
2026-06-09 18:04:12 +0300

    refactor route definitions for improved organization and readability

--- routes/web.php
+++ routes/web.php
@@ -9,27 +9,20 @@ use App\Http\Controllers\DashboardController;
 
 Route::get('/', fn () => redirect()->route('dashboard'));
 
-/* Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
- */
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('users', UserController::class);
+
+    Route::resource('tasks', TaskController::class);
+    Route::get('/tasks/{task}/start',    [TaskController::class, 'start'])->name('tasks.start');
+    Route::get('/tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::resource('categories', CategoryController::class);
 });
-
-Route::resource('users', UserController::class);
-
-
-
-Route::resource('tasks', TaskController::class);
-Route::get('/tasks/{task}/start',    [TaskController::class, 'start'])->name('tasks.start');
-Route::get('/tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
-
- Route::get('/dashboard', [DashboardController::class, 'index'])
-        ->name('dashboard');
-
-Route::resource('categories', CategoryController::class);
 
 require __DIR__.'/auth.php';
